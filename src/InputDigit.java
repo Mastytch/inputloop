7ea1f89c3b15c233e52803deadb850d0f47e3024
@mastytch <andriy.mostovyy@gmail.com>
2015-02-20 22:59:29 +0200

    player goes to another class

--- src/InputDigit.java
+++ src/InputDigit.java
@@ -1,25 +1,32 @@
+/**
+ * Created by mastytch and Azazavr on 20.02.2015.
+ */
 import java.util.Scanner;
 
-/**
- * Created by mastytch on 20.02.2015.
- */
 public class InputDigit {
     static int goal;
     static int guess;
     static Scanner keyboard = new Scanner(System.in);
     static Scanner newGame = new Scanner(System.in);
     static Scanner getPlayerName = new Scanner(System.in);
+    static Player p1 = new Player();
+
 
     public static void main(String[] args) {
+
+        System.out.print("Привет, давай познакомимся. Меня зовут Мегамозг! А тебя? (Введите свое имя): ");
+        p1.setName(getPlayerName.nextLine());
         playGame();
     }
+
+
+
 
     public static void playGame() {
         goal = (int) (Math.random() * 99 + 1);
         int numberOfTries = 8;
-        System.out.println("Привет, давай познакомимся. Меня зовут Мегамозг! А тебя? (Введите свое имя): ");
-        String playerName = getPlayerName.next();
-        System.out.printf("Уважаемый(-ая) %s, я загадал число от 1 до 100. Попробуй-ка его угадать. Осторожно, попыток всего %d!\n", playerName, numberOfTries);
+
+        System.out.printf("%s, я загадал число от 1 до 100. Попробуй-ка его угадать. Осторожно, попыток всего %d!\n", p1.getName(), numberOfTries);
         //System.out.println("Компьютер загадал: " + goal);//Строка для дебага. Потом убрать
 
         do {
@@ -27,13 +34,12 @@ public class InputDigit {
             guess = keyboard.nextInt();
             //System.out.println("Вы ввели: " + guess); //Строка для дебага. Потом убрать
             if (guess == goal) {
-                System.out.printf("Поздравляем, %s! Вы угадали! Количество оставшихся попыток: %d.\n", playerName, numberOfTries);
+                System.out.printf("Поздравляю, %s! Вы угадали! Количество оставшихся попыток: %d.\n", p1.getName(), numberOfTries);
                 System.out.println("Хотите попробовать еще раз? Введите 1, если ДА и 0, если НЕТ");
                 int ng = newGame.nextInt();
                 if (ng == 1) {
                     playGame();
                 } else {
-                    numberOfTries = 0;
                     break;
                 }
             } else if (guess > goal && numberOfTries > 0) {
@@ -43,13 +49,12 @@ public class InputDigit {
                 --numberOfTries;
                 System.out.println("Введенное число меньше загаданного. Осталось попыток: " + numberOfTries);
             } else if (numberOfTries == 0) {
-                System.out.printf("К сожалению, %s, вы проиграли!\n", playerName);
+                System.out.printf("ХАХАХА, Мегамозг победил! %s, вы проиграли!\n", p1.getName());
                 System.out.println("Хотите попробовать еще раз? Введите 1, если ДА и 0, если НЕТ");
                 int ng = newGame.nextInt();
                 if (ng == 1) {
                     playGame();
                 } else {
-                    numberOfTries = 0;
                     break;
                 }
             }
---
+++ src/Player.java
@@ -0,0 +1,19 @@
+/**
+ * Created by mastytch and Azazavr on 20.02.2015.
+ */
+
+public class Player{
+
+    private String playerName;
+
+    public void setName (String playerName){
+
+        this.playerName = playerName;
+    }
+
+    public String getName() {
+        return playerName;
+    }
+
+}
+
