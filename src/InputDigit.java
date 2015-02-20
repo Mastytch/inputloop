fff6dc220026bbd854eb38abddf7efb850d7e3b7
@mastytch <andriy.mostovyy@gmail.com>
2015-02-20 15:22:47 +0200

    added fail when run out of tries

--- src/InputDigit.java
+++ src/InputDigit.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 /**
@@ -10,15 +9,17 @@ public class InputDigit {
     static Scanner keyboard = new Scanner(System.in);
     static Scanner newGame = new Scanner(System.in);
     static Scanner getPlayerName = new Scanner(System.in);
+
     public static void main(String[] args) {
         playGame();
-        }
-        public static void playGame(){
+    }
+
+    public static void playGame() {
         goal = (int) (Math.random() * 99 + 1);
-        int numberOfTries = 1;
-            System.out.println("Привет, давай познакомимся. Меня зовут Мегамозг! А тебя (Введите свое имя): ");
+        int numberOfTries = 8;
+        System.out.println("Привет, давай познакомимся. Меня зовут Мегамозг! А тебя? (Введите свое имя): ");
         String playerName = getPlayerName.next();
-        System.out.printf("Уважаемый(-ая) %s, я загадал число от 1 до 100. Попробуй-ка его угадать!\n", playerName);
+        System.out.printf("Уважаемый(-ая) %s, я загадал число от 1 до 100. Попробуй-ка его угадать. Но будь осторожен, количество попыток ограничего, всего %d!\n", playerName, numberOfTries);
         //System.out.println("Компьютер загадал: " + goal);//Строка для дебага. Потом убрать
 
         do {
@@ -26,21 +27,32 @@ public class InputDigit {
             guess = keyboard.nextInt();
             //System.out.println("Вы ввели: " + guess); //Строка для дебага. Потом убрать
             if (guess == goal) {
-                System.out.printf("Поздравляем, %s! Вы угадали c %d попытки!\n", playerName, numberOfTries);
+                System.out.printf("Поздравляем, %s! Вы угадали! Количество оставшихся попыток: %d.\n", playerName, numberOfTries);
                 System.out.println("Хотите попробовать еще раз? Введите 1, если ДА и 0, если НЕТ");
                 int ng = newGame.nextInt();
-                if(ng == 1){
+                if (ng == 1) {
                     playGame();
-                }else{
+                } else {
+                    numberOfTries = 0;
                     break;
                 }
-            } else if (guess > goal) {
-                numberOfTries++;
-                System.out.println("Введенное число больше загаданного.");
-            } else if (guess < goal) {
-                numberOfTries++;
-                System.out.println("Введенное число меньше загаданного.");
+            } else if (guess > goal && numberOfTries > 0) {
+                --numberOfTries;
+                System.out.println("Введенное число больше загаданного. Осталось попыток: " + numberOfTries);
+            } else if (guess < goal && numberOfTries > 0) {
+                --numberOfTries;
+                System.out.println("Введенное число меньше загаданного. Осталось попыток: " + numberOfTries);
+            } else if (numberOfTries == 0) {
+                System.out.printf("К сожалению, %s, вы проиграли!\n", playerName);
+                System.out.println("Хотите попробовать еще раз? Введите 1, если ДА и 0, если НЕТ");
+                int ng = newGame.nextInt();
+                if (ng == 1) {
+                    playGame();
+                } else {
+                    numberOfTries = 0;
+                    break;
+                }
             }
-        }while (guess != goal);
+        }while (numberOfTries > 0|| guess != goal) ;
     }
 }
